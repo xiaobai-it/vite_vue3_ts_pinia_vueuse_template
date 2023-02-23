b1bdbcc6d0e06ff9d41b8fb9f416aff55a076c20
qwd <qwd@northking.net>
2023-02-23 12:42:02 +0800

    feature(all): 解决elint+prettier起冲突的问题5

--- src/view/vueUse.vue
+++ src/view/vueUse.vue
@@ -59,7 +59,7 @@ const myClick = () => {
 	globalProperties.$message.success('123')
 	globalProperties.$message({
 		type: 'success',
-		message: '1213',
+		message: '121312313123',
 		// center: true,
 	})
 	console.log(globalProperties)
